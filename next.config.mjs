d35f706a83f8f13ad544529297b7ccf016575595
Kay <92441528+kay0218@users.noreply.github.com>
2026-04-19 19:57:52 +0100

    fixing deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = "/book-proj-1";
+
 const nextConfig = {
     output: "export",
+    basePath: repoName,
+    assetPrefix: repoName,
     images: {
       unoptimized: true,
     },
-  };;
+  };
 
 export default nextConfig;

676e90eb44fec8c480e0b7d5f7b2d1fb43be1501
Kay <92441528+kay0218@users.noreply.github.com>
2026-04-19 20:19:26 +0100

    setup github pages actions deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const repoName = "/book-proj-1";
 
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: repoName,
-  assetPrefix: repoName,
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
+    output: "export",
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;

7e06303ef28ac7eb3a0f026de5f5287ca7a28ed2
Kay <92441528+kay0218@users.noreply.github.com>
2026-08-23 12:10:09 +0100

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: "export",
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+  output: "export",
+  trailingSlash: true,
+  basePath: "/book-proj-1",
+  assetPrefix: "/book-proj-1/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
